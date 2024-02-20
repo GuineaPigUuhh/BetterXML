b3d76eb10fc193b121813e272cd07e534ecca2dc
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2024-02-20 11:51:54 -0300

    Fixessss and more Description

--- xmlhelper/XmlHelper.hx
+++ xmlhelper/XmlHelper.hx
@@ -2,7 +2,15 @@ package xmlhelper;
 
 using StringTools;
 
-enum XmlCanTypes {
+/**
+ * These are the types that XML can parse
+ * @param STR
+ * @param INT
+ * @param FLOAT
+ * @param BOOL
+ * @param DYNAMIC
+ */
+enum XmlTypes {
     STR;
     INT;
     FLOAT;
@@ -10,7 +18,47 @@ enum XmlCanTypes {
     DYNAMIC;
 }
 
-class XmlUtil {
+/**
+ * change the writing type of the string
+ * @param NONE
+ * @param UPPERCASE
+ * @param LOWERCASE
+ */
+enum XmlStringEffects {
+    NONE;
+    UPPERCASE;
+    LOWERCASE;
+}
+
+/**
+ * Functions that help you with XML.
+ */
+@haxe.warning("XmlHelper Class has not yet been tested")
+class XmlHelper {
+
+    /**
+     * This function is useless but it can be nice to organize, it is only used for these functions within this class, but I let you use it
+     * @param xml your XML Class
+     * @param att the variable you want to get from the XML to make Parse
+     * @param effect change the writing type of the string
+     * @param def When parse fails it will return this value 
+     * @return String
+     */
+    public static function getString(xml:Xml, att:String, ?effect:XmlStringEffects = NONE, ?def:String = ''):String {
+        if (xml.get(att) != null)
+        {
+            var daStr = xml.get(att);
+            switch (effect)
+            {
+                case LOWERCASE: daStr = daStr.toLowerCase();
+                case UPPERCASE: daStr = daStr.toUpperCase();
+                default:
+            }
+            return daStr;
+        }    
+        return def; 
+    }
+
     /**
      * Look for the value you want in XML and format it to bool
      * @param xml your XML Class
@@ -19,8 +67,8 @@ class XmlUtil {
      * @return Bool
      */
     public static function getBool(xml:Xml, att:String, ?def:Bool = false):Bool {
-        if (xml.exists(att))
-            return Simplify.parseBool(xml.get(att).toLowerCase());
+        if (xml.get(att) != null)
+            return Simplify.parseBool(getString(xml, att, LOWERCASE));
         return def;
     }
 
@@ -29,9 +77,10 @@ class XmlUtil {
      * @param xml your XML Class
      * @param att the variable you want to get from the XML to make Parse
      * @param def When parse fails it will return this value
+     * @return Float or Int
      */
     public static function getNumber(xml:Xml, att:String, ?def:Float = 0) {
-        if (xml.get(att).contains('.'))
+        if (getString(xml, att).contains('.'))
             return getFloat(xml, att);
         else
             return getInt(xml, att);
@@ -43,9 +92,10 @@ class XmlUtil {
      * @param xml your XML Class
      * @param att the variable you want to get from the XML to make Parse
      * @param def When parse fails it will return this value
+     * @return Float
      */
     public static function getFloat(xml:Xml, att:String, ?def:Float = 0) {
-        if (xml.exists(att))
+        if (xml.get(att) != null)
             return Std.parseFloat(xml.get(att));
         return def;
     }
@@ -55,9 +105,10 @@ class XmlUtil {
      * @param xml your XML Class
      * @param att the variable you want to get from the XML to make Parse
      * @param def When parse fails it will return this value
+     * @return Int
      */
     public static function getInt(xml:Xml, att:String, ?def:Int = 0) {
-        if (xml.exists(att))
+        if (xml.get(att) != null)
             return Std.parseInt(xml.get(att));
         return def;
     }
@@ -66,11 +117,12 @@ class XmlUtil {
      * Look for the value you want in the XML and identify the type of the variable and the function applies it to a type it thinks it is, It is not recommended that you use
      * @param xml your XML Class
      * @param att the variable you want to get from the XML to make Parse
+     * @return Dynamic
      */
     @:haxe.warning("It is not recommended that you use")
     public static function getDynamic(xml:Xml, att:String):Dynamic {
         var numberArray = [for (i in 0...9) Std.string(i)];
-        var boolAtt = xml.get(att).toLowerCase();
+        var boolAtt = getString(xml, att, LOWERCASE);
         if ((boolAtt == "false" || boolAtt == "0") || (boolAtt == "true" || boolAtt == "1"))
             return getBool(xml, att);
         else if (Simplify.containsArray(att, numberArray))
@@ -85,21 +137,21 @@ class XmlUtil {
      * @param att the variable you want to get from the XML to make Parse
      * @param arrayType will format each type variable you choose
      * @param splot value that the function will use to separate the Variable string from the XML
+     * @return Array<Dynamic>
      */
-    public static function getArray(xml:Xml, att:String, ?arrayType:XmlCanTypes = STR, ?splot:String = ',') {
+    public static function getArray(xml:Xml, att:String, ?arrayType:XmlTypes = STR, ?splot:String = ',') {
         var array:Array<Dynamic> = [];
-        if (xml.exists(att))
+        if (xml.get(att) != null)
         {
             array = xml.get(att).split(splot);
             for (i in array)
             {
                 switch (arrayType)
                 {
-                    case STR: 
                     case BOOL: i = Simplify.parseBool(i);
                     case FLOAT: i = Std.parseFloat(i);
-                    case INT: i = Std.parseFloat(i);
-                    case DYNAMIC:
+                    case INT: i = Std.parseInt(i);
+                    default: i = Std.string(i);
                 }
             }    
         }    
